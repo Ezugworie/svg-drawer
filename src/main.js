98639d96e97be2fadb92aaa6518a08709cd887a5
Ikechukwu Ezugworie <iyke.jaypee@yahoo.com>
2021-01-09 19:26:18 +0100

    chore: add sqaure and rectangle drawer

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { router } from './router'
+import { store } from './store/index'
+import { VuelidatePlugin } from '@vuelidate/core'
+// import Vivus from 'vivus'
 
 const app = createApp(App)
 
 
 app.use(router)
+app.use(store)
+app.use(VuelidatePlugin)
+// app.use(Vivus)
 
 app.mount('#app')
